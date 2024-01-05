d4b48c88ddce137632679167189d28b7dc887abf
Seth McNeill <seth.mcneill@erau.edu>
2024-01-05 09:40:27 -0700

    2024 Spring days

--- countDays.py
+++ countDays.py
@@ -9,21 +9,20 @@
 from datetime import datetime, timedelta
 import numpy as np
 
-startDate = datetime.strptime('8/29/2022','%m/%d/%Y')  # can also use the datetime(YYYY,mm,dd) method
-endDate = datetime.strptime('12/9/2022','%m/%d/%Y')  # np.arange does not include last date
+startDate = datetime.strptime('01/10/2024','%m/%d/%Y')  # can also use the datetime(YYYY,mm,dd) method
+endDate = datetime.strptime('04/26/2024','%m/%d/%Y')  # np.arange does not include last date
 dt = timedelta(days=1)
 nonSchoolDays = (
-  datetime(2022,9,5),    # MLK
-  datetime(2022,10,6),   # Fall Expo
-  datetime(2022,10,13),  # Fall Break
-  datetime(2022,10,14),  # Fall Break
-  datetime(2022,11,11),  # Veterans day
-  datetime(2022,11,23),  # Thanksgiving
-  datetime(2022,11,24),  # Thanksgiving
-  datetime(2022,11,25),  # Thanksgiving
-  datetime(2022,11,26),  # Thanksgiving
+  datetime(2024,1,15),    # MLK Jr day
+  datetime(2024,2,19),    # Presidents day
+  datetime(2024,3,11),  # Thanksgiving
+  datetime(2024,3,12),  # Thanksgiving
+  datetime(2024,3,13),  # Thanksgiving
+  datetime(2024,3,14),  # Thanksgiving
+  datetime(2024,3,15),  # Thanksgiving
   )
 
+print(f'School starts: {startDate:%m/%d/%Y}')
 daySum = np.zeros(7)
 
 # Looping through dates method from:
@@ -33,6 +32,9 @@ for day in np.arange(startDate,endDate,dt).astype(datetime):
 
 for day in nonSchoolDays:
   daySum[day.weekday()] -= 1
+  print(f'No school on {day:%m/%d/%Y}')
+
+print(f'Reading day is: {endDate:%m/%d/%Y}')
 
 # print(daySum)
 
@@ -43,3 +45,4 @@ print(f'MWF meetings: {mwfDays}')
 print(f'TTH meetings: {trDays}')
 print(f'T only: {daySum[1]}')
 print(f'TH only: {daySum[3]}')
+print(daySum)
